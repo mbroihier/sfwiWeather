eb9f0768f21ed48a3c3a90d3496d7c3a1fcbe47c
mbroihier <mbroihier@yahoo.com>
2019-11-30 23:37:20 +0000

    handle query errors

--- weather.js
+++ weather.js
@@ -55,8 +55,21 @@ var updateJSONObject = function () {
 	    //console.log("Response from query:"+replyDOM.serialize());
 	    let replyDocument = replyDOM.window.document;
 	    //console.log("Document portion:", replyDocument.body.textContent);
-	    let replyJSON = JSON.parse(replyDocument.body.textContent); // make JSON object
-	    JSONObject = replyJSON;
+	    let replyJSON = null;
+	    try {
+		replyJSON = JSON.parse(replyDocument.body.textContent); // make JSON object
+		if ("properties" in replyJSON) {
+		    JSONObject = replyJSON;
+		}
+	    } catch (err) {
+		console.log("JSON parse error:", err);
+	    }
+	    if (JSONObject == null) {
+	        lastUpdateTime = new Date();
+		updateInProgress = false;
+		console.log("JSONObject not initialized");
+		return;
+	    }
             for (let forecastObject of JSONObject.properties.periods) {
 	      let rowTS = new Date(forecastObject.startTime);
 		forecastObject.innerHTML = dayArray[rowTS.getDay()] + ((rowTS.getHours() < 10) ? "0":"") + rowTS.getHours() + ":" + ((rowTS.getMinutes() < 10) ? "0" : "") + rowTS.getMinutes();
@@ -151,43 +164,45 @@ app.get("/", function(request, response, next) {
     // fill in forecast table and estimate temperature at time of table generation
     let insertionPoint = dom.window.document.querySelector("#forecastTable");
     let count = 0;
-    for (let forecastObject of JSONObject.properties.periods) {
-	if ((count % 6) == 0) {
-	    let tableRow = dom.window.document.createElement("tr");
-	    let tableCellDate = dom.window.document.createElement("td");
-	    tableCellDate.innerHTML = forecastObject.innerHTML;
-	    tableRow.appendChild(tableCellDate);
-	    let tableCellTemp = dom.window.document.createElement("td");
-	    tableCellTemp.innerHTML = forecastObject.temperature;
-	    tableCellTemp.setAttribute('style', 'text-align:center');
-	    tableRow.appendChild(tableCellTemp);
-	    let tableCellWind = dom.window.document.createElement("td");
-	    tableCellWind.innerHTML = forecastObject.windSpeed;
-	    tableRow.appendChild(tableCellWind);
-	    let tableCellDesc = dom.window.document.createElement("td");
-	    tableCellDesc.innerHTML = forecastObject.shortForecast;
-	    tableRow.appendChild(tableCellDesc);
-	    insertionPoint.appendChild(tableRow);
+    if (JSONObject != null) {
+	for (let forecastObject of JSONObject.properties.periods) {
+	    if ((count % 6) == 0) {
+		let tableRow = dom.window.document.createElement("tr");
+		let tableCellDate = dom.window.document.createElement("td");
+		tableCellDate.innerHTML = forecastObject.innerHTML;
+		tableRow.appendChild(tableCellDate);
+		let tableCellTemp = dom.window.document.createElement("td");
+		tableCellTemp.innerHTML = forecastObject.temperature;
+		tableCellTemp.setAttribute('style', 'text-align:center');
+		tableRow.appendChild(tableCellTemp);
+		let tableCellWind = dom.window.document.createElement("td");
+		tableCellWind.innerHTML = forecastObject.windSpeed;
+		tableRow.appendChild(tableCellWind);
+		let tableCellDesc = dom.window.document.createElement("td");
+		tableCellDesc.innerHTML = forecastObject.shortForecast;
+		tableRow.appendChild(tableCellDesc);
+		insertionPoint.appendChild(tableRow);
+	    }
+	    count = count + 1;
 	}
-	count = count + 1;
-    }
-    let temperature = JSONObject.properties.periods[0].temperature;
-    let windSpeed = JSONObject.properties.periods[0].windSpeed;
-    let direction = JSONObject.properties.periods[0].windDirection;
-    let asciiTemperature = temperature + "\xB0F";
-    let elements = dom.window.document.querySelectorAll("p");
-    for (let element of elements) {
-	if (element.getAttribute('name') == 'temperature') {
-	    element.innerHTML = asciiTemperature;
-	} else if (element.getAttribute('name') == 'description') {
-	    element.innerHTML = JSONObject.properties.periods[0].shortForecast;
-	} else if (element.getAttribute('name') == 'wind') {
-	    element.innerHTML = direction + " @ " + windSpeed;
-	} else if (element.getAttribute('name') == 'time') {
-	    let dateString = new Date().toString();
-	    element.innerHTML = pattern.exec(dateString)[0];
-	} else if (element.getAttribute('name') == 'range') {
-	    element.innerHTML = temperatureRange[dayArray[(new Date()).getDay()]+"00:00"];
+	let temperature = JSONObject.properties.periods[0].temperature;
+	let windSpeed = JSONObject.properties.periods[0].windSpeed;
+	let direction = JSONObject.properties.periods[0].windDirection;
+	let asciiTemperature = temperature + "\xB0F";
+	let elements = dom.window.document.querySelectorAll("p");
+	for (let element of elements) {
+	    if (element.getAttribute('name') == 'temperature') {
+		element.innerHTML = asciiTemperature;
+	    } else if (element.getAttribute('name') == 'description') {
+		element.innerHTML = JSONObject.properties.periods[0].shortForecast;
+	    } else if (element.getAttribute('name') == 'wind') {
+		element.innerHTML = direction + " @ " + windSpeed;
+	    } else if (element.getAttribute('name') == 'time') {
+		let dateString = new Date().toString();
+		element.innerHTML = pattern.exec(dateString)[0];
+	    } else if (element.getAttribute('name') == 'range') {
+		element.innerHTML = temperatureRange[dayArray[(new Date()).getDay()]+"00:00"];
+	    }
 	}
     }
     response.send(dom.serialize());
